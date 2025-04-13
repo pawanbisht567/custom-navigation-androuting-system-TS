c1b67c46aa27c175c8edf078844d4c64ae75dd65
pbisht17 <pawanbisht94.pb@gmail.com>
2025-04-13 17:06:58 +0530

    New changes for the Protals

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,17 +1,35 @@
 import ReactDOM from "react-dom";
-
+import { useEffect } from "react";
 interface ModalProps {
   onClose: () => void;
   children?: React.ReactNode;
+  actionBar: React.ReactNode;
 }
 
-export default function Modal({ onClose, children }: ModalProps) {
+export default function Modal({ onClose, children, actionBar }: ModalProps) {
+
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden');
+
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    }
+      
+  }, []);
+
   const modalRoot = document.querySelector('.modal-container') as Element | null;
   
   return modalRoot ? ReactDOM.createPortal(
   <div>
-    <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-    <div className="absolute inset-40 p-10 bg-white">{children}</div>
+    <div onClick={onClose} className="fixed inset-0 bg-gray-300 opacity-80"></div>
+    <div className="fixed inset-40 p-10 bg-white">
+      <div className="flex flex-col justify-between h-full">
+        {children}
+        <div className="flex justify-end">
+        {actionBar}
+      </div>
+      </div>
+    </div>
   </div>,
   modalRoot
   ) : null
